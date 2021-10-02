5eade6d94a48a950a6022c57eb6f884582afe9a6
AHTepsilon <alehertoro725@gmail.com>
2021-10-02 14:03:20 -0500

    added progress

---
+++ src/Principal.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import processing.core.PApplet;
+
+public class Principal extends PApplet
+{
+
+	public static void main(String[] args) 
+	{
+		PApplet.main("Principal");
+	}
+	
+	@Override
+	public void settings() //void Awake
+	{
+		size(800, 500);
+	}
+	
+	BufferedWriter writer;
+	BufferedReader reader;
+	
+	private Socket socket;
+	
+	@Override
+	public void setup() //void Start
+	{
+		initServer();
+	}
+	
+	@Override
+	public void draw() //void Update
+	{		
+		background(255);
+	}
+	
+	public void initServer()
+	{
+		new Thread(
+				() -> 
+				{
+					try {
+						ServerSocket server = new ServerSocket(4000);
+						System.out.println("Awaiting Connection...");
+						socket = server.accept();
+						System.out.println("Client Connected");
+						
+						InputStream is = socket.getInputStream();
+						InputStreamReader isr = new InputStreamReader(is);
+						reader = new BufferedReader(isr);
+						
+						OutputStream os = socket.getOutputStream();
+						OutputStreamWriter osw = new OutputStreamWriter(os);
+						writer = new BufferedWriter(osw);
+						
+						while(true)
+						{
+							System.out.println("Awaiting message...");
+							String line = reader.readLine();
+							System.out.println("Received message: " + line);
+						}
+						
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}).start();
+	}
+
+}
